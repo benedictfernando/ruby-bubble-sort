b11a7e44d8a9863c0609f8ff27f48193414ccfc1
benedictfernando <fernandobenedict619@gmail.com>
2022-03-20 15:00:38 +0800

    finish bubble_sort

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,1 +1,21 @@
-puts "hello world!"
+def bubble_sort(array)
+     
+    return array if array.size <= 1
+
+    unsorted = true
+    while unsorted do
+        unsorted = false
+        0.upto(array.size - 2) do |i|
+            if array[i] > array[i + 1]
+                array[i], array[i + 1] = array[i + 1], array[i]
+                unsorted = true
+            end
+        end
+    end
+
+    # return array
+    array
+end
+
+array = [4,3,78,2,0,2]
+p bubble_sort(array)
